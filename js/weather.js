e771e6dfc85be115aacaee97ba14abff1d2d96fb
mdhCodes <mdhcodes@gmail.com>
2022-02-28 16:17:30 -0500

    Refactor code, update site, and change api

--- js/weather.js
+++ js/weather.js
@@ -1,89 +1,108 @@
-/* Get the user's current date with MomentJS*/
 
-var dateAndTime = moment().format('dddd') + ', ' + moment().format('MMMM Do, YYYY, h:mm a');
-document.getElementById("date").innerHTML = dateAndTime;
+/* Get the user's current date with MomentJS*/
+const date = document.getElementById('date');
+const dateAndTime = moment().format('dddd') + ', ' + moment().format('MMMM Do, YYYY, h:mm a');
+date.textContent = dateAndTime;
 
 
 /* Get the user's current location with the geolocation API */
-
-var getLocation = function() {
+const getLocation = function() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude;
-            var lon = position.coords.longitude;
-            getWeather(lat, lon);
-        });
+            const lat = position.coords.latitude.toFixed(4); // Allow only 4 decimal places
+            const lon = position.coords.longitude.toFixed(4);
+            // console.log('lat', lat);
+            // console.log('lon', lon);
+            getUrl(lat, lon);
+        });        
     }
 };
 
 
-/* Get the weather for the user's current location with the Weather Underground API */
+const getUrl = (lat, lon) => {
+    fetch(`https://api.weather.gov/points/${lat},${lon}`)
+        .then(response => {
+            return response.json();            
+        })
+        .then(response => {            
+            const url = response.properties.forecast;
+            const city = response.properties.relativeLocation.properties.city;
+            const state = response.properties.relativeLocation.properties.state;
+            const location = document.getElementById('location');
+            location.textContent = `${city}, ${state}`;
+            // console.log('url: ', url)
 
-var getWeather = function(lat, lon) {
-    var req = new XMLHttpRequest();
-    var url = "https://api.wunderground.com/api/0126420f39b48bf0/conditions/q/" + lat + "," + lon + ".json";
-    req.open("GET", url, true);
-    req.onload = function(response) {
-        if(req.status === 200 && req.statusText === "OK") {
-            response = JSON.parse(req.responseText);
-            var location = response['current_observation']['display_location']['full'];
-            var tempC = response['current_observation']['temp_c'];
-            var tempF = response['current_observation']['temp_f'];
-            var description = response['current_observation']['weather'];
-            var icon = response['current_observation']['icon_url'];
-            var humidity = response['current_observation']['relative_humidity'];
-            var feelsLikeC = response['current_observation']['feelslike_c'];
-            var feelsLikeF = response['current_observation']['feelslike_f'];
-            var windDirection = response['current_observation']['wind_dir'];
-            var creditTitle = response['current_observation']['image']['title'];
-            var creditImage = response['current_observation']['image']['url'];
-            var creditLink = response['current_observation']['image']['link'];
-            document.getElementById("location").innerHTML = location;
-            document.getElementById("current-temp").innerHTML = 'Now: ' + Math.round(tempC) + '°';
-            document.getElementById("description").innerHTML = description;
-            document.getElementById("icon").innerHTML = '<img src=' + icon + '>';
-            document.getElementById("feels-like").innerHTML = 'Feels Like: ' + Math.round(feelsLikeC) + '°';
-            document.getElementById("humidity").innerHTML = 'Humidity: ' + humidity;
-            document.getElementById("wind-direction").innerHTML = 'Wind Direction: ' + windDirection;
-            document.getElementById("credit").innerHTML = '<img src=' + creditImage + '><p>Weather delivered by: <a href=' + creditLink + 'target="_blank">' + creditTitle + ' API</a></p>';
-        } else {
-            var error = document.getElementById("error");
-            error.innerHTML = "An error occurred: Please refresh the page.";
-            error.style.color = "#f00";
-            error.style.border = "solid";
-        }
-        // Passing local variables to the convertTemp function
-        convertTemp(tempC, tempF, feelsLikeC, feelsLikeF);
-    };
-    req.send();
-};
+            getWeather(url);
+        })
+        .catch(error => console.log('error: ', error));
+}
 
 
 /* Convert celcius to fahrenheit */
-
-var convertTemp = function(tempC, tempF, feelsLikeC, feelsLikeF) {
-    var convert = false;
+const convertTemp = function(tempF) {
+    const tempC = Math.round((tempF - 32) * 5 / 9);
+    let convert = false;
     document.getElementById("convert-temp").onclick = function() {
         if(convert) {
-            document.getElementById("current-temp").innerHTML = 'Now: ' + Math.round(tempC) + '°';
-            document.getElementById("feels-like").innerHTML = 'Feels Like: ' + Math.round(feelsLikeC) + '°';
-            document.getElementById("convert-temp").innerHTML = "C";
-            document.getElementById("convert-temp").setAttribute("title", "Click to convert to Fahrenheit");
+            document.getElementById("current-temp").textContent = `${Math.round(tempF)}\u00B0`;
+            document.getElementById("convert-temp").textContent = "F";
+            document.getElementById("convert-temp").setAttribute("title", "Click to convert to Celcius");
             convert = false;
         } else {
-            document.getElementById("current-temp").innerHTML = 'Now: ' + Math.round(tempF) + '°';
-            document.getElementById("feels-like").innerHTML = 'Feels Like: ' + Math.round(feelsLikeF) + '°';
-            document.getElementById("convert-temp").innerHTML = "F";
-            document.getElementById("convert-temp").setAttribute("title", "Click to convert to Celcius");
+            document.getElementById("current-temp").textContent = `${Math.round(tempC)}\u00B0`;
+            document.getElementById("convert-temp").textContent = "C";
+            document.getElementById("convert-temp").setAttribute("title", "Click to convert to Fahrenheit");
             convert = true;
         }
     }
 };
 
 
+const getWeather = (url) => {
+    fetch(url)
+        .then(response => {
+            if (response.status === 200 && response.statusText === "OK") {
+                return response.json();
+            } else {
+                const errorMessage = document.getElementById('error');
+                errorMessage.textContent = 'An error occurred: Please refresh the page.'
+            }
+        })
+        .then(response => {
+            const currentTemp = document.getElementById('current-temp');
+            const description = document.getElementById('description');
+            const icon = document.getElementById('icon');
+            const credit = document.getElementById('credit');
+            const creditImg = document.getElementById('credit-img'); 
+            const tempF = response.properties.periods[0].temperature;
+            const tempUnit = response.properties.periods[0].temperatureUnit; 
+            const timeOfDay = response.properties.periods[0].name;
+            const shortForecast = response.properties.periods[0].shortForecast;
+            const detailedForecast = response.properties.periods[0].detailedForecast;
+            const weatherImg = response.properties.periods[0].icon;
+            const creditTitle = 'National Weather Service';
+            const creditImage = './nws.png';
+            const creditLink = 'https://www.weather.gov/documentation/services-web-api';
+
+            currentTemp.textContent = `${tempF}\u00B0`;
+            description.innerHTML = '<p><span class="conditions">Current Conditions</span>' + detailedForecast + '</p>';
+            icon.innerHTML = '<img src=' + weatherImg + '>';
+                       
+            credit.innerHTML = '<p>Weather delivered by: <a href=' + creditLink + 'target="_blank">' + creditTitle + ' API</a></p>';
+            creditImg.innerHTML = '<img src=' + creditImage + '>';
+
+            const design = document.getElementById('design');
+            design.innerHTML = '<p class="design">Design inspired by: <a href="https://weather.com/weather/today/l/e98a878cd02dd90c43e4567f4cb5b1824f77886502447b0fc7136cfe697d7d6f" target="_blank">The Weather Channel</a></p>';
+                
+            const convertTemperature = document.getElementById('convert-temp');
+            convertTemperature.addEventListener('click', convertTemp(tempF)); 
+        })
+        .catch(error => console.log('error: ', error));
+}
+
 
 window.onload = function() {
     getLocation();
+    getUrl();
     getWeather();
-    convertTemp();
 };
